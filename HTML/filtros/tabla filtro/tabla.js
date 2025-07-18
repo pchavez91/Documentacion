0f0dfcbff1dcfefc23230306ef1f7205e8a25196
PatricioChavez <pchavez.dev@gmail.com>
2025-07-18 11:56:55 -0400

    filtros en html

---
+++ HTML/filtros/tabla filtro/tabla.js
@@ -0,0 +1,28 @@
+<script>
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+</script>
+</script>
+
+//elimine toUpperCase() si desea realizar una búsqueda que distinga entre mayúsculas y minúsculas.
+
+//cambie tr[i].getElementsByTagName('td') [0] a [1] si desea buscar "País" (índice 1) en lugar de "Nombre" (índice 0).
