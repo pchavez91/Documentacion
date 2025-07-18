61f2101c20a6420ea599177ab252e16d1faa5942
PatricioChavez <pchavez.dev@gmail.com>
2025-07-18 10:45:38 -0400

    mas funciones en html

--- Javascript/documento.js
+++ Javascript/documento.js
@@ -10,3 +10,21 @@ window.onclick = function(event) {
   }
 }
 </script>
+
+//detectar bloq mayus
+// Get the input field
+var input = document.getElementById("myInput");
+
+// Get the warning text
+var text = document.getElementById("text");
+
+// When the user presses any key on the keyboard, run the function
+input.addEventListener("keyup", function(event) {
+
+  // If "caps lock" is pressed, display the warning text
+  if (event.getModifierState("CapsLock")) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none"
+  }
+});
